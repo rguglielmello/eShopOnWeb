9c814b3e55e1475bb41b01f4a19e0859e6689163
Rafael Andrade <rdeandrade@microsoft.com>
2022-11-25 09:34:09 -0300

    solution init

---
+++ src/Web/Extensions/HttpClientHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.eShopWeb.Web.Extensions;
+
+public class HttpClientHelper : HttpClient
+{
+    public HttpClientHelper(string header, string value)
+    {
+        if (header == null)
+            throw new ArgumentNullException(nameof(header));
+        if (value == null)
+            throw new ArgumentNullException(nameof(value));
+
+        DefaultRequestHeaders.Add(header, value); 
+    }
+}
